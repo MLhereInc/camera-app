62f67bfd2965f0b4c94fe4506558fa182fcd1755
Alireza Hadj Khodabakhshi <akhodabakhshi@webroot.com>
2020-03-11 22:06:02 -0700

    adding post

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#shoot")
+    submitTrigger = document.querySelector("#submit")
+    returnTrigger = document.querySelector("#return")
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -25,5 +28,20 @@ cameraTrigger.onclick = function() {
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
+
+submitTrigger.onclick = function() {
+    data = cameraOutput.src
+    
+    $.post( "http://ec2-50-18-222-52.us-west-1.compute.amazonaws.com:9012/inference", data, function( ret ) {
+        alert( ret );
+    });
+
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
