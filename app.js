4cdf88d4aaf2ddc9d7fb4776efeaa27f15f9efa7
Alireza Hadj Khodabakhshi <akhodabakhshi@webroot.com>
2020-03-11 22:10:13 -0700

    adding post

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ cameraTrigger.onclick = function() {
 };
 
 submitTrigger.onclick = function() {
+    alert("submit")
     data = cameraOutput.src
     
     $.post( "http://ec2-50-18-222-52.us-west-1.compute.amazonaws.com:9012/inference", data, function( ret ) {
